ffc380b066a08eae0d6588512ea8f4c7e72a618b
Ye Yang Sheng <piaoxu1993@gmail.com>
2015-09-03 17:46:19 +0800

    bug fix

--- controllers/cashinvoice.js
+++ controllers/cashinvoice.js
@@ -168,7 +168,7 @@ exports.showUserInvoice = function (req, res, next) {
         var totalMoney = 0;
         for (var i = 0; i < totalInvoices; i++) {
           totalMoney += invoices[i].totalPrice;
-          if (i === 0) {
+          if (i === 0 && i < totalInvoices - 1) {
             invoices[i].prev = '';
             invoices[i].next = invoices[i+1]._id;
           } else if (i > 0 && i < totalInvoices - 1) {
@@ -271,7 +271,7 @@ exports.showAllInvoice = function (req, res, next) {
       var totalMoney = 0;
       for (var i = 0; i < totalInvoices; i++) {
         totalMoney += invoices[i].totalPrice;
-        if (i === 0) {
+        if (i === 0 && i < totalInvoices - 1) {
           invoices[i].prev = '';
           invoices[i].next = invoices[i+1]._id;
         } else if (i > 0 && i < totalInvoices - 1) {
